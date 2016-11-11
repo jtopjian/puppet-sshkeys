7572b070bec4a461064ec727907d5a8b9139168c
Francois Laupretre <francois@tekwire.net>
2016-11-12 00:33:25 +0100

    Protect against non-existing user (and other additions)
    When user did not exist or home not published yet, key was wrongly (and quietly) generated in /.ssh
    Easily unnoticed because, on the next run, a new key would be generated in the right dir
    Also add a new optional 'ssh_dir' parameter. This allows to create the user and generate the key during the same run, and to generate a key in a different directory (knowing it won't be exported).
    Add a '$require' parameter. This allows to control dependencies, for instance, against the resource creating the user.

--- manifests/create_ssh_key.pp
+++ manifests/create_ssh_key.pp
@@ -10,8 +10,14 @@
 #   [*group*]
 #     The group owner of the homedir.
 #
+#   [*ssh_dir*]
+#     Directory where the key will be created. Defaults to '~user/.ssh'.
+#     Also allows to create the user and the key in the same agent run
+#     (when the user was just created, the home dir fact is not available
+#     before the next run)
+#
 #   [*create_ssh_dir*]
-#     Whether to create /home/user/.ssh.
+#     Whether to create the directory.
 #
 #   [*ssh_keytype*]
 #     Either rsa or dsa.
@@ -26,65 +32,84 @@
 define sshkeys::create_ssh_key (
   $owner          = undef,
   $group          = undef,
+  $ssh_dir        = undef,
   $create_ssh_dir = true,
   $ssh_keytype    = 'rsa',
   $ssh_bitlength  = undef,
   $passphrase     = '',
+  $require        = undef,
 ) {
 
   validate_bool($create_ssh_dir)
   validate_string($ssh_keytype)
 
-  $homedir = getvar("::home_${name}")
-
-  # Set $bitlength to default if no value provided
-  $rsa_default = '2048'
-  $dsa_default = '1024'
-
-  if $ssh_bitlength {
-    $bitlength = $ssh_bitlength
+  if ($ssh_dir != undef) {
+    $dir = $ssh_dir
   } else {
-    case $ssh_keytype {
-      'rsa': {
-        $bitlength = $rsa_default
-      }
-      'dsa': {
-        $bitlength = $dsa_default
-      }
-      default: {
-        fail('The sshkeys module currently supports only rsa or dsa default bit lengths')
+    $homedir = getvar("::home_${name}")
+    if ($homedir == undef) {
+	  notify { "Cannot determine the home dir of user '${name}'. Skipping SSH key creation": }
+	  $dir = undef
+	} else {
+	  $dir = "${homedir}/.ssh"
+	}
+  }
+
+  if ($dir != undef) {  
+    # Set $bitlength to default if no value provided
+    $rsa_default = '2048'
+    $dsa_default = '1024'
+  
+    if $ssh_bitlength {
+      $bitlength = $ssh_bitlength
+    } else {
+      case $ssh_keytype {
+        'rsa': {
+          $bitlength = $rsa_default
+        }
+        'dsa': {
+          $bitlength = $dsa_default
+        }
+        default: {
+          fail('The sshkeys module currently supports only rsa or dsa default bit lengths')
+        }
       }
     }
-  }
-
-  if $owner {
-    $owner_real = $owner
-  } else {
-    $owner_real = $name
-  }
-
-  if $group {
-    $group_real = $group
-  } else {
-    $group_real = $name
-  }
-
-  if $create_ssh_dir {
-    file { "${homedir}/.ssh":
-      ensure => directory,
-      owner  => $owner_real,
-      group  => $group_real,
-      mode   => '0700',
+  
+    if $owner {
+      $owner_real = $owner
+    } else {
+      $owner_real = $name
     }
-    $require = File["${homedir}/.ssh"]
-  } else {
-    $require = undef
-  }
-
-  exec { "ssh_keygen-${name}":
-    command => "/usr/bin/ssh-keygen -t ${ssh_keytype} -b ${bitlength} -f '${homedir}/.ssh/id_${ssh_keytype}' -N '${passphrase}' -C '${name}@${::fqdn}'",
-    user    => $name,
-    creates => "${homedir}/.ssh/id_${ssh_keytype}",
-    require => $require,
+  
+    if $group {
+      $group_real = $group
+    } else {
+      $group_real = $name
+    }
+  
+    if $create_ssh_dir {
+      file { "${dir}":
+        ensure => directory,
+        owner  => $owner_real,
+        group  => $group_real,
+        mode   => '0700',
+		require => $require,
+      }
+	  if ($require == undef) {
+		$required = File["${dir}"]
+      } else {
+		$required = [ $require, File["${dir}"] ].flatten
+	  }
+    } else {
+      $required = $require
+    }
+  
+    exec { "ssh_keygen-${name}":
+      command => "/usr/bin/ssh-keygen -t ${ssh_keytype} -b ${bitlength} -f '${dir}/id_${ssh_keytype}' -N '${passphrase}' -C '${name}@${::fqdn}'",
+      user    => $name,
+      creates => "${dir}/id_${ssh_keytype}",
+      require => $required,
+    }
   }
 }
