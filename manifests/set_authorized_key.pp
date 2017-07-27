db5e79fc8d97261d0573d6c39d781bd0b01f33a2
Elyse Salberg <elyse_salberg@putnam.com>
2017-07-27 17:52:01 -0400

    Allow overriding the downcase of the remote node in case PuppetDB has it stored as uppercase
    add results_node

--- manifests/set_authorized_key.pp
+++ manifests/set_authorized_key.pp
@@ -19,18 +19,32 @@
 #   [*target*]
 #     The destination authorized_keys file.
 #
+#   [*downcase*]
+#     Whether to downcase the remote_node name in the query request.
+#     The results_node name returned will always be lowercase.
+#     Default: true
+#
 define sshkeys::set_authorized_key (
   $local_user,
   $remote_user,
-  $ensure  = 'present',
-  $options = undef,
-  $target  = undef
+  $ensure   = 'present',
+  $options  = undef,
+  $target   = undef,
+  $downcase = true
 ) {
 
   # Parse the name
   $parts = split($remote_user, '@')
   $remote_username = $parts[0]
-  $remote_node     = downcase($parts[1])
+
+  if $downcase {
+    $remote_node   = downcase($parts[1])
+    $results_node  = $remote_node
+  }
+  else {
+    $remote_node   = $parts[1]
+    $results_node  = downcase($parts[1])
+  }
 
   $home = getvar("::home_${local_user}")
   if ($home == undef) {
@@ -42,12 +56,12 @@ define sshkeys::set_authorized_key (
     } else {
       $target_real = "${home}/.ssh/authorized_keys"
     }
-    
+
     Ssh_authorized_key {
       user   => $local_user,
       target => $target_real,
     }
-    
+
     if $ensure == 'absent' {
       ssh_authorized_key { $name:
         ensure => absent,
@@ -59,8 +73,8 @@ define sshkeys::set_authorized_key (
       } else {
         $results = query_facts("hostname=\"${remote_node}\"", ["sshpubkey_${remote_username}"])
       }
-      if is_hash($results) and has_key($results, $remote_node) {
-        $key = $results[$remote_node]["sshpubkey_${remote_username}"]
+      if is_hash($results) and has_key($results, $results_node) {
+        $key = $results[$results_node]["sshpubkey_${remote_username}"]
         if ($key !~ /^(ssh-...) ([^ ]*)/) {
           err("Can't parse key from ${remote_user}")
           notify { "Can't parse key from ${remote_user}. Skipping": }
